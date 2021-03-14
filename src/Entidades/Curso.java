3e91b8c2a4e99ea1c3af4cb6d826cb394db5f2f8
diego <diego@debian.monge.com>
2021-03-13 21:11:43 -0600

    estructura inicial proyecto final de progra 2

---
+++ src/Entidades/Curso.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author diego
+ */
+public class Curso {
+    
+    //tipo de curso> lab o teoria
+    //profesores (si es lab son 2)
+    //costo
+    //limite 15 lab, 20 teoria
+    //capacidad actual (segun la matricula)
+}
---
+++ src/Entidades/Persona.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author diego
+ */
+public class Persona {
+    
+    private String nombre;
+    private String apellido;
+    private String identificacion;
+    private String correo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+    
+}
