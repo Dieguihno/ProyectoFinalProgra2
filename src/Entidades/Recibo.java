72dd30d48bca4f36794574b4dc3022a3973768e9
diego <diego@debian.monge.com>
2021-03-13 21:03:44 -0600

    estructura inicial proyecto final de progra 2

---
+++ src/Entidades/Recibo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author diego
+ */
+public class Recibo {
+    
+    //impuesto de ventas
+    //impuesto a la educacion
+    //total 
+    // descuento: si el promedio ponderado es 10 se le hace 20% de descuento
+    // si el promedio ponderado es de 9.5 el descuento es de 10%
+    
+    
+}
