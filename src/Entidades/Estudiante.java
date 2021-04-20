bc8bab17637b398bdd17befd46a946692e546b4b
diego <diego@debian.monge.com>
2021-04-19 22:23:06 -0600

    Clases y metodos de estudiante listos

---
+++ src/Entidades/Estudiante.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author diego
+ */
+public class Estudiante {
+    
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private double promedio;
+    private String carrera;
+    private String clave;
+    private String usuario;
+
+    public Estudiante(String nombre, String apellido, String correo, double promedio, String carrera, String clave, String usuario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.promedio = promedio;
+        this.carrera = carrera;
+        this.clave = clave;
+        this.usuario = usuario;
+    }
+
+    public Estudiante() {
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+    
+}
